12737c559fb6d5f9b054d6bad1823135035224a5
Michael Green <mgreen@fanatics.com>
2019-10-06 21:30:25 -0500

    dun

--- Week3/EMS.py
+++ Week3/EMS.py
@@ -6,11 +6,11 @@ employee_list = []
 
 def AddEmployee():
     employee = []
-    employee.append(input('Enter employee name: '))
-    employee.append(input('Enter employee\'s social security number: '))
-    employee.append(input('Enter employee\'s phone number: '))
-    employee.append(input('Enter employee\'s email address: '))
-    employee.append(input('Enter employee\'s salary: '))
+    employee.append(str(input('Enter employee name: ')))
+    employee.append(str(input('Enter employee\'s social security number: ')))
+    employee.append(str(input('Enter employee\'s phone number: ')))
+    employee.append(str(input('Enter employee\'s email address: ')))
+    employee.append(str(input('Enter employee\'s salary: ')))
     employee_list.append(employee)
 
 def RemoveEmployee():
@@ -21,11 +21,11 @@ def ViewEmployees():
         print("{}, {}, {}, {}, ${}".format(e[0],e[1],e[2],e[3],e[4]))
 
 def PrintStatus():
-    pass
-
+    print("Hi! Welcome to the Acme Employee Management System! There are currently " + str(len(employee_list)) + " employees in the system!")
 
 while True:
-    i = input("Enter: 1 to add an employee; 2 to view all employees; 3 to view the status of the system: ; 4 to exit ")
+    i = str(input("Enter: 1 to add an employee; 2 to view all employees; 3 to view the status of the system: ; 4 to exit "))
+    print("You entered: " + i)
     if i == "1":
         AddEmployee()
     elif i == "2":
@@ -33,4 +33,5 @@ while True:
     elif i == "3":
         PrintStatus()
     elif i == "4":
+        print("Goodbye!")
         exit()
